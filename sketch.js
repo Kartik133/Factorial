f13ef3a1f15db8988b13eca31e6c27d9b8829742
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2022-01-02 16:47:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,129 @@
-var gameState = 1,x,y,a,b,c;
+var a,b,c,d,e,f,g,h,i,k;
+var gameState = "start";
+var x,xx;
+var j = -20;
+
+function preload() {
+  g =  loadAnimation("e.png");
+  f =  loadAnimation("a.png");
+  d =  loadAnimation("a.png","b.png","c.png","d.png","e.png","f.png","g.png","h.png");
+  i = loadSound("toss.mp3");
+  k = loadSound("collide.mp3");
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
-  a = createInput("");
-  b = createButton("set");
-  c = createButton("reset");
+  frameRate(100);
 
-  a.position((width/2)-50,(height/2)-10);
-  b.position((width/2)-50,(height/2)+20);
-  c.position((width/2)-50,(height/2)+70);
+  a = createSprite(width/2,height-15,width-10,20);
+  a.shapeColor = rgb(0,0,0);
 
+  e = createSprite(width/2,height-65,width-10,20);
+  e.addAnimation("dd",g);
+  e.addAnimation("ddd",f);
+  e.addAnimation("dddd",d);
+  e.scale = 0.7;
+  
+  
+  b = createButton("HEADS");
+  c = createButton("TAILS");
+  h = createButton("TOSS AGAIN");
+
+  b.position(20,height-90);
+  c.position(width-130,height-90);
+  h.position(width-250,height-100);
+  
+ // b.style('width', '160px');
+ // b.style('height', '80px');
+  b.style('background', 'orange');
+  b.style('font-size', '40px');
+
+ // c.style('width', '160px');
+ // c.style('height', '80px');
+  c.style('background', 'orange');
+  c.style('font-size', '40px');
+
+ // h.style('width', '200px');
+ // h.style('height', '100px');
+  h.style('background', 'orange');
+  h.style('font-size', '40px');
 }
 
 function draw() {
-  background(0);
+  background(255);
 
-  if(gameState===1) {
-    a.show();
+  if(gameState==="start") {
+    h.hide();
+
     b.show();
-    c.hide();
-
-    b.mousePressed(()=>{
-      x = (int(a.value()))-1;
-      y = int(a.value());
-      gameState = 2;
-    });
-  }
-  
-  if(gameState===2){
-    a.hide();
-    b.hide();
     c.show();
 
-    c.mousePressed(()=>{
-      x = 0;
-      y = 0;
-      gameState = 1;
+    b.mousePressed(()=>{
+      x = "heads";
+      i.play();
+      gameState = "spin";
     });
 
-    noStroke();
-    fill(255);
-    text(y,width/2,height/2);
+    c.mousePressed(()=>{
+      x = "tails";
+      i.play();
+      gameState = "spin";
+    });
+  }
+
+  if(gameState==="spin") {
+    b.hide();
+    c.hide();
     
-    if(x!==0) {
-      y = x*y;
-      x-=1;
+    e.changeAnimation("dddd",d);
+    e.velocityY = j;
+    j+=0.6;
+
+    if(e.collide(a)) {
+      rand = Math.round(random(1,2));
+      switch(rand) {
+        case 1:e.changeAnimation("ddd",f);
+               xx = "heads";
+               k.play();
+               gameState = "reset";
+               break;
+        case 2:e.changeAnimation("dd",g);
+               xx = "tails"; 
+               k.play();
+               gameState = "reset";
+               break;
+        default:break;
+      }
     }
   }
+
+  e.collide(a);
+
+  if(gameState==="reset") {
+    h.show();
+
+    if(x===xx) {
+      textAlign(CENTER);
+      textSize(120);
+      noStroke();
+      fill(0);
+      text("You Won",width/2,height/2);
+    }else{
+       textAlign(CENTER);
+       textSize(120);
+       noStroke();
+       fill(0);
+       text("You Lost",width/2,height/2);
+     }
+
+    h.mousePressed(()=>{
+      j = -20;
+      gameState = "start";
+    });
+  }
+
+  //text(mouseX+";"+mouseY,400,400);
+
+  drawSprites();
 }
